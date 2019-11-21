1bcb416f90204890b5d175ffff0c60df22a8d6a3
Alan <57141737+aaaa1a1a@users.noreply.github.com>
2019-11-22 00:13:59 +0800

    Adding draw_time_series function

--- data_visualization.py
+++ data_visualization.py
@@ -4,3 +4,19 @@ Implement generic methods for the following:
 - Charts of returns calculated in feature_engineering.py
 
 '''
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from statsmodels.tsa.seasonal import seasonal_decompose
+
+#Function to generate decomposition graph
+#index: source from Data file, eg. 'SP_1d_10y.csv'
+#col: 'Close','Open','Volume',...
+def draw_time_series(index, col):
+    data = pd.read_csv('data/'+index)
+    name = index.split('.')[0]
+    data = data.set_index('Date')
+    decom = seasonal_decompose(data[col], freq=int(len(data)/10))
+    decom.plot()
+    plt.savefig('Charts/'+name+'_'+col+'_decom.png')
+    
